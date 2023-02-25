b1db7180c0837279d8801df35549e3f3769f4939
Alex <amoahtnt6@gmail.com>
2023-02-25 21:57:56 +0000

    Done with assertion and type-casting

--- l-5/build/js/main.js
+++ l-5/build/js/main.js
@@ -3,3 +3,13 @@
 let a = 'Alex';
 let b = a; //less specific
 let d = 5;
+// The dom
+const img = document.querySelector('img');
+const myImg = document.getElementById('#img');
+img.src;
+myImg.src;
+// testing my knowledge
+const year = document.getElementById('year');
+const thisYear = new Date().getFullYear().toString();
+year.setAttribute('dateTime', thisYear);
+year.textContent = thisYear;
--- l-5/src/main.ts
+++ l-5/src/main.ts
@@ -9,3 +9,19 @@ let a: one = 'Alex'
 let b = a as two; //less specific
 
 let d = <two> 5 ;
+
+// The dom
+const img = document.querySelector('img')!
+const myImg =<HTMLImageElement> document.getElementById('#img')
+
+img.src
+
+myImg.src 
+
+// testing my knowledge
+
+
+const year = <HTMLElement> document.getElementById('year')
+const thisYear = new Date().getFullYear().toString();
+year.setAttribute('dateTime',thisYear)
+year.textContent = thisYear;
