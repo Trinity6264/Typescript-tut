31b8932e7a3b0d1dc36e926ba6c7125a752bfed4
Alex <amoahtnt6@gmail.com>
2023-02-27 16:18:47 +0000

    Done with key assertion

--- l-7/build/js/main.js
+++ l-7/build/js/main.js
@@ -1,2 +1,17 @@
 "use strict";
 // Index signature
+const todayTransactions = {
+    Books: -10,
+    Job: true,
+    Pizza: 300
+};
+console.log(todayTransactions.Books);
+let prop = 'Pizza';
+console.log(todayTransactions);
+class Human {
+    constructor(type, age) {
+        this.type = type;
+        this.age = age;
+        this.type = type;
+    }
+}
--- l-7/src/main.ts
+++ l-7/src/main.ts
@@ -1,2 +1,40 @@
 // Index signature
 
+interface Transaction {
+    readonly [index: string] :  number | boolean
+
+}
+
+const todayTransactions: Transaction = {
+    Books: -10,
+    Job: true,
+    Pizza: 300
+}
+
+console.log(todayTransactions.Books);
+
+
+let prop: string = 'Pizza';
+console.log( todayTransactions );
+
+
+///////////////////////////////////
+
+type LivingThing = {
+   
+    type: 'human' | 'Animal'
+}
+
+
+class Human implements LivingThing{
+
+    constructor(
+        public type: 'human',
+        public age: number
+        
+    ){
+        this.type = type;
+    }
+    
+
+}
